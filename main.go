a4975fa276efbef559dc5e8cacdfb041aa822c4f
normen <normen667@gmail.com>
2023-11-30 22:41:08 +0100

    bump version

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/normen/obs-mcu/obs"
 )
 
-var VERSION string = "v0.5.0"
+var VERSION string = "v0.5.1"
 var waitGroup sync.WaitGroup
 
 // TODO: config file command line option
