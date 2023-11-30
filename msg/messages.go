3f9bed30dc54a0b60c7fbd289212fc2f906cac60
normen <normen667@gmail.com>
2023-11-30 10:07:08 +0100

    messages: code annotations

--- msg/messages.go
+++ msg/messages.go
@@ -1,124 +1,108 @@
 package msg
 
-type ControlMessage struct {
-	Type  string
-	Value string
+// obs <- mackie
+type UpdateRequest struct {
 }
 
-type StateMessage struct {
-	Type  string
-	Value string
-}
-
-// sent by mackie to obs
-
+// obs <- mackie
 type KeyMessage struct {
 	HotkeyName string
 }
 
-type UpdateRequest struct {
+// obs <- mackie
+type BankMessage struct {
+	ChangeAmount int
 }
 
-// sent by obs to mackie
+// obs <- mackie
+type VPotChangeMessage struct {
+	FaderNumber  byte
+	ChangeAmount int
+}
 
-type RecMessage struct {
+// obs <- mackie
+type VPotButtonMessage struct {
 	FaderNumber byte
-	Value       bool
 }
 
-type SoloMessage struct {
-	FaderNumber byte
-	Value       bool
-}
-
+// obs <-> mackie
 type MuteMessage struct {
 	FaderNumber byte
 	Value       bool
 }
 
+// obs <-> mackie
 type SelectMessage struct {
 	FaderNumber byte
 	Value       bool
 }
 
+// obs <-> mackie
 type AssignMessage struct {
 	Mode byte
 }
 
+// obs <-> mackie
 type TrackEnableMessage struct {
 	TrackNumber byte
 	Value       bool
 }
 
+// obs <-> mackie
 type FaderMessage struct {
 	FaderNumber byte
 	FaderValue  float64
 }
 
+// obs <-> mackie
 type MonitorTypeMessage struct {
 	FaderNumber byte
 	MonitorType string
 }
 
+// obs -> mackie
 type LedMessage struct {
 	LedName  string
 	LedState bool
 }
 
-type TextMessage struct {
-	Text  string
-	Lower bool
-	Start byte
-	End   byte
-}
-
+// obs -> mackie
 type ChannelTextMessage struct {
 	FaderNumber byte
 	Text        string
 	Lower       bool
 }
 
+// obs -> mackie
 type DisplayTextMessage struct {
 	Text string
 }
 
-type MeterMessage struct {
-	FaderNumber byte
-	Value       float64
-}
-
-type BankMessage struct {
-	ChangeAmount int
-}
-
-type VPotChangeMessage struct {
-	FaderNumber  byte
-	ChangeAmount int
-}
-
-type VPotButtonMessage struct {
-	FaderNumber byte
-}
-
+// obs -> mackie
 type VPotLedMessage struct {
 	FaderNumber byte
 	LedState    byte
 }
 
+// obs -> mackie
 type AssignLEDMessage struct {
 	Characters []rune
 }
 
-// internal mcu messages
+// obs -> mackie
+type MeterMessage struct {
+	FaderNumber byte
+	Value       float64
+}
+
+// internal mcu message
 type RawFaderMessage struct {
 	FaderNumber byte
 	FaderValue  int16
 }
 
+// internal mcu message
 type RawFaderTouchMessage struct {
 	FaderNumber byte
 	Pressed     bool
-}
-
-type UserShortcutRequest struct {
 }
